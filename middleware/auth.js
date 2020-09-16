e3e1e7a9b22d86126df88ac34327018a7717a8e6
IAN-GREY <401022707@qq.com>
2020-09-16 20:05:20 +0800

    2020/9/16

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,21 +2,17 @@
  * @Description: 
  * @Author: 沈林圩
  * @Date: 2020-09-09 10:29:18
- * @LastEditTime: 2020-09-09 14:28:38
+ * @LastEditTime: 2020-09-16 11:44:36
  * @LastEditors: 沈林圩
  */
-
 const jwt = require('jwt-simple');
 const auth = async (req, res, next) => {
-  if (req.url != '/user/login' && req.url != '/api/imgCode' && req.url != '/api/lone' && req.url != '/user/register') {
+  if (req.url != '/user/login') {
     var token = (req.body && req.body.access_token) || (req.query && req.query.access_token) || req.headers['x-access-token']
     if (token) {
       try {
         var decoded = jwt.decode(token, app.get('jwtTokenSecret'));//解码token
-        req.query.account = decoded.account;//通过token获取account加入get请求参数
-        req.body.account = decoded.account;//通过token获取account加入post请求参数
         if (decoded.exp <= Date.now()) {
-          // res.end('Access token has expired', 400)
           res.json({
             code: "400",
             data: "Token was out of date",
@@ -24,14 +20,21 @@ const auth = async (req, res, next) => {
           });
         }
         else if (decoded.account) {
-          mdb.collection('user').findOne({ account: decoded.account }, function (err, user) {
-            // req.user_id = user._id;
-            next()
+          mdb.collection('users').findOne({ account: decoded.account }, function (err, user) {
+            if (user) {
+              req.query.account = decoded.account;//通过token获取account加入get请求参数
+              req.body.account = decoded.account;//通过token获取account加入post请求参数
+              next()
+            } else {
+              res.json({
+                code: "400",
+                data: "Token was not recognised",
+                msg: "invalid_token",
+              });
+            }
           })
         }
       } catch (err) {
-        // return next()
-        // res.end('Access err', 400)
         res.json({
           code: "400",
           data: "Token was not recognised",
@@ -39,7 +42,6 @@ const auth = async (req, res, next) => {
         });
       }
     } else {
-      // res.end('  no Access ', 400)
       res.json({
         code: "400",
         data: "Token was required",
@@ -50,6 +52,5 @@ const auth = async (req, res, next) => {
   else {
     next();
   }
-
 }
 module.exports = auth
--- user.js
+++ user.js
@@ -2,22 +2,36 @@
  * @Description: 
  * @Author: 沈林圩
  * @Date: 2020-08-24 12:48:29
- * @LastEditTime: 2020-09-10 14:40:52
+ * @LastEditTime: 2020-09-16 11:30:50
  * @LastEditors: 沈林圩
  */
 const express = require("express");
 const router = express.Router();
 const path = require('path');
+const multer = require('multer');
 var fs = require('fs')
 var moment = require('moment');
 var jwt = require('jwt-simple');
 const { Users } = require('./model/model')
 const auth = require('./middleware/auth')
-
+var storage = multer.diskStorage({
+  destination: path.join(__dirname, '/uploads')
+});
+var upload = multer({ storage: storage });
+var chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+function generateMixed (n) {
+  var res = "";
+  for (var i = 0; i < n; i++) {
+    var id = Math.ceil(Math.random() * 35);
+    res += chars[id];
+  }
+  return res;
+}
 router.post("/register", function (req, res) {
   let param = {
     account: req.body.account,
     password: req.body.password,
+    avatar: '',
   }
   var date = new Date();
   var year = date.getFullYear();
@@ -27,6 +41,7 @@ router.post("/register", function (req, res) {
   var minute = date.getMinutes();
   var second = date.getSeconds();
   param['created_time'] = year + '年' + month + '月' + day + '日' + hour + ':' + minute + ':' + second
+  param['nickname'] = '用户' + generateMixed(10)
   Users.findOne({ account: req.body.account }, function (err, result) {
     if (err) throw err;
     if (result) {
@@ -48,12 +63,41 @@ router.post("/register", function (req, res) {
             code: 200,
             token: token,
             account: result.account,
+            nickname: result.nickname,
             expires: expires,
             msg: '注册成功'
           });
         }
       });
-
+    }
+  });
+});
+router.post("/auth", auth, function (req, res) {
+  var account = req.body.account;
+  Users.findOne({ account: account }, function (err, result) {
+    if (err) throw err;
+    if (result) {
+      var expires = moment().add(7, 'days').valueOf();
+      var token = jwt.encode({
+        account: result.account,
+        exp: expires,
+      }, app.get('jwtTokenSecret'));
+      res.json({
+        code: 200,
+        msg: '登录成功',
+        token: token,
+        expires: expires,
+        data: {
+          account: result.account,
+          nickname: result.nickname,
+          avatar: result.avatar,
+        }
+      })
+    } else {
+      res.json({
+        code: 400,
+        msg: 'token非法！'
+      });
     }
   });
 });
@@ -75,6 +119,7 @@ router.post("/login", function (req, res) {
         expires: expires,
         data: {
           account: result.account,
+          nickname: result.nickname,
         }
       })
     } else {
@@ -85,25 +130,100 @@ router.post("/login", function (req, res) {
     }
   });
 });
-router.post("/avatar", function (req, res) {
-  const filepath = path.join(__dirname, '/uploads/' + req.body.account + '_avatar.png')
-  console.log('???', req.files)
-  fs.writeFile(filepath, req.files[0].path, (err) => {
+router.post("/avatar", upload.single('file'), auth, function (req, res) {
+  let name = req.body.account + '_avatar_' + new Date().getTime() + '.png'
+  const destpath = path.join(__dirname, '/uploads/' + name)
+  try {
+    fs.readFile(req.file.path, (err, data) => {
+      fs.writeFile(destpath, data, (err) => {
+        res.json({
+          code: 200,
+          data: {
+            avatar: name
+          },
+          msg: '上传成功'
+        });
+        fs.unlink(req.file.path, (err) => {
+        })
+      });
+    });
+  } catch (error) {
+    console.log(error)
+    res.json({
+      code: 201,
+      msg: '上传失败'
+    });
+  }
+
+});
+
+router.post("/update", auth, function (req, res) {
+  let param = {
+    $set: {
+
+    }
+  }
+  if (req.body.avatar) {
+    param.$set['config_data'] = req.body.avatar
+  }
+  if (req.body.nickname) {
+    param.$set['title'] = req.body.nickname
+  }
+  Users.updateOne({ "account": req.body.account }, param, function (err, result) {
     if (err) {
       res.json({
-        code: 200,
-        data: {
-          avatar: req.body.account + '_avatar.png'
-        },
-        msg: '上传失败'
+        code: 201,
+        msg: '修改失败'
       });
-    } else {
+      throw err
+    };
+    if (result) {
       res.json({
         code: 200,
-        data: {
-          avatar: req.body.account + '_avatar.png'
-        },
-        msg: '上传成功'
+        msg: '修改成功'
+      });
+    }
+  });
+});
+router.post("/newPassword", auth, function (req, res) {
+  let param = {
+    $set: {
+
+    }
+  }
+  if (req.body.newPassword) {
+    param.$set['password'] = req.body.newPassword
+  }
+  Users.findOne({ account: req.body.account }, function (err, result) {
+    if (err) throw err;
+    if (result) {
+      if (result.password !== req.body.oldPassword) {
+        res.json({
+          code: 201,
+          msg: '密码错误'
+        });
+        return
+      }
+      Users.updateOne({ "account": req.body.account }, param, function (err, result) {
+        if (err) {
+          res.json({
+            code: 201,
+            msg: '修改失败'
+          });
+          throw err
+        };
+        if (result) {
+          res.json({
+            code: 200,
+            msg: '修改成功'
+          });
+        }
+      });
+    }
+    else {
+      res.json({
+        code: 201,
+        msg: '账号不存在'
       });
     }
   });
